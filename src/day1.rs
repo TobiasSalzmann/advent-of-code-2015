ec46e57e3840af18fb6d4cf5f67b44b8a7b7c62e
tsalzman <tsalzman@thoughtworks.com>
2022-12-01 21:13:22 +0100

    Day 1 Co-authored-by: Fiona Coath <ficoath@gmail.com>

--- src/day1.rs
+++ src/day1.rs
@@ -1,20 +1,62 @@
 use crate::util;
 
 pub fn main() {
-    let input = util::parse_int_lines("resources/day1.txt");
-    print!("{:?}", input)
+    let input = util::parse_int_lists("resources/day1.txt");
+
+    println!("Day 1, Part 1: {:?} calories", max_calories(input.clone()));
+    println!("Day 1, Part 2: {:?} calories", max_3_calories(input));
 }
 
-pub fn foo(a: i32) -> i32 {
-    a + 1
+fn max_calories(calorie_lists: Vec<Vec<i32>>) -> i32 {
+    calorie_lists.iter()
+        .map(|calorie_list| calorie_list.iter().sum())
+        .max()
+        .unwrap()
+}
+
+fn max_3_calories(calorie_lists: Vec<Vec<i32>>) -> i32 {
+    let mut calorie_totals = calorie_lists.iter()
+        .map(|calorie_list| calorie_list.iter().sum())
+        .collect::<Vec<i32>>();
+    calorie_totals.sort_by(|a, b| b.cmp(a));
+    calorie_totals.iter()
+        .take(3)
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day1::foo;
+    use crate::day1::{max_3_calories, max_calories};
 
     #[test]
-    fn foo_incerements_int() {
-        assert_eq!(foo(665), 666);
+    fn find_max_total_of_lists() {
+        let calorie_lists: Vec<Vec<i32>> = vec![
+            vec![43],
+            vec![2,20,22],
+            vec![1,1,1,1,1]
+        ];
+        assert_eq!(max_calories(calorie_lists), 44);
     }
+
+    #[test]
+    fn find_max_total_of_simple_lists() {
+        let calorie_lists: Vec<Vec<i32>> = vec![
+            vec![43],
+        ];
+        assert_eq!(max_calories(calorie_lists), 43);
+    }
+
+
+    #[test]
+    fn find_top_3_total_of_simple_lists() {
+        let calorie_lists: Vec<Vec<i32>> = vec![
+            vec![1],
+            vec![1,1,1,1],
+            vec![1,1],
+            vec![1,1,1],
+        ];
+        assert_eq!(max_3_calories(calorie_lists), 9);
+    }
+
+
 }
--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,19 @@
 use std::fs;
 
-pub fn parse_int_lines(file_path: &str) -> Vec<i32> {
+// pub fn parse_int_lines(file_path: &str) -> Vec<i32> {
+//     let contents = fs::read_to_string(file_path).expect("File does not exists");
+//     let lines = contents.lines();
+//     let numbers: Vec<i32> = lines
+//         .filter_map(|s| s.parse::<i32>().ok())
+//         .collect();
+//     numbers
+// }
+
+pub fn parse_int_lists(file_path: &str) -> Vec<Vec<i32>> {
     let contents = fs::read_to_string(file_path).expect("File does not exists");
-    let lines = contents.lines();
-    let numbers: Vec<i32> = lines
-        .filter_map(|s| s.parse::<i32>().ok())
+    let lines: Vec<&str> = contents.lines()
         .collect();
-    numbers
+    lines.split(|s| s.is_empty())
+        .map(|ss| ss.iter().filter_map(|s| s.parse::<i32>().ok()).collect())
+        .collect()
 }

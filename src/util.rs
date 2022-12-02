ee30488352830e1f448acca497f4baf8bd1f8e2f
tsalzman <tsalzman@thoughtworks.com>
2022-12-02 16:52:13 +0100

    Add test for lists parsing

--- src/util.rs
+++ src/util.rs
@@ -17,3 +17,29 @@ pub fn parse_int_lists(file_path: &str) -> Vec<Vec<i32>> {
         .map(|ss| ss.iter().filter_map(|s| s.parse::<i32>().ok()).collect())
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::io::Write;
+    use crate::util::{parse_int_lists};
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn parses_list() {
+
+        let mut file: NamedTempFile = NamedTempFile::new().expect("Failed to create file");
+        file.write_all("43\n\n2\n20\n22\n\n1\n1\n1\n1\n1\n".as_bytes()).expect("Faile to write to file");
+        let filename = file.path().to_str().expect("Failed to get file path");
+
+        let lists = parse_int_lists(filename);
+
+        let expected_lists: Vec<Vec<i32>> = vec![
+            vec![43],
+            vec![2, 20, 22],
+            vec![1, 1, 1, 1, 1],
+        ];
+
+        assert_eq!(lists, expected_lists);
+    }
+}

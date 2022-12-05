39e6dbe5f939bcd71f07e85383ab728c4e2d5b59
tsalzman <tsalzman@thoughtworks.com>
2022-12-05 22:34:51 +0100

    Day 5 Co-authored-by: Fiona Coath <ficoath@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 extern crate dotenv;
+extern crate core;
 
 use dotenv::dotenv;
 use std::env;
@@ -22,6 +24,7 @@ fn main() {
         2 => day2::main(),
         3 => day3::main(),
         4 => day4::main(),
+        5 => day5::main(),
         _ => {println!("Not yet implemented 😅")}
     }
 }

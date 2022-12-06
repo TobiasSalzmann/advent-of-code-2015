3f21397afeec9085e6c53b9f2c7b5531cd6c1611
tsalzman <tsalzman@thoughtworks.com>
2022-12-06 22:03:20 +0100

    Day 6 Co-authored-by: Fiona Coath <ficoath@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 extern crate dotenv;
 extern crate core;
@@ -25,6 +26,7 @@ fn main() {
         3 => day3::main(),
         4 => day4::main(),
         5 => day5::main(),
+        6 => day6::main(),
         _ => {println!("Not yet implemented 😅")}
     }
 }

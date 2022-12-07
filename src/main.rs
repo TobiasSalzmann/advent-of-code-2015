d5ab616ec15d188bae39c220021c1877004c34f7
tsalzman <tsalzman@thoughtworks.com>
2022-12-07 20:10:17 +0100

    Day 7

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 extern crate dotenv;
 extern crate core;
@@ -27,6 +28,7 @@ fn main() {
         4 => day4::main(),
         5 => day5::main(),
         6 => day6::main(),
+        7 => day7::main(),
         _ => {println!("Not yet implemented 😅")}
     }
 }

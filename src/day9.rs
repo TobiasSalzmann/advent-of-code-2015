57cd9cd6cb168bf60e507f78fedf6d89afa57c28
tsalzman <tsalzman@thoughtworks.com>
2022-12-14 18:06:35 +0100

    Day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,4 @@
+use std::ops::Deref;
 use std::str::FromStr;
 use itertools::{fold, Itertools, repeat_n};
 use petgraph::visit::Walker;
@@ -7,88 +8,82 @@ pub fn main() {
     let input: Vec<Action> = util::parse_from_strings("resources/day9.txt");
 
     println!(
-        "Day 11, Part 1: {:?}", part1(input.clone())
+        "Day 9, Part 1: {:?}", part(input.clone(), 2)
     );
 
-    // println!(
-    //     "Day 11, Part 2:\n{}", part2(input)
-    // );
+    println!(
+        "Day 9, Part 2: {:?}", part(input.clone(), 10)
+    );
 }
 
-fn part1(actions: Vec<Action>) -> usize {
-    let mut states = vec![State { head_pos: (0, 0), tail_pos: (0, 0) }];
+fn part(actions: Vec<Action>, number_of_knots: usize) -> usize {
+    let mut states = vec![State { knots: (1..=number_of_knots).map(|_| (0,0)).collect_vec() }];
     let flat_actions = actions.iter()
         .flat_map(|Action { dir, times }| (1..=times.clone()).map(|_| Action { dir: dir.clone(), times: 1 }));
 
     for a in flat_actions {
-        let new_state = states.last().unwrap().move_head(a).move_tail();
+        let new_state = states.last().unwrap().advance(a);
         states.push(new_state)
     }
 
     states.iter()
-        .map(|s| s.tail_pos)
+        .map(|s| s.tail_pos())
         .unique()
         .count()
 }
 
-
-// fn part2(commands: Vec<Command>) -> String {
-//     let v = compute_signal(commands);
-//
-//     v.iter()
-//         .take(240)
-//         .enumerate()
-//         .map(|(i, n)| {
-//             let j = (i as i32) % 40;
-//             if j >= n-1 && j <= n+1 {'#'} else {'.'}
-//         })
-//         .chunks(40).into_iter()
-//         .map(|cs| cs.into_iter().join(""))
-//         .join("\n")
-// }
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct State {
-    head_pos: (i32, i32),
-    tail_pos: (i32, i32),
+    knots: Vec<(i32, i32)>
 }
 
 impl State {
-    fn move_head(&self, action: Action) -> State {
-        let (mut head_x, mut head_y) = self.head_pos;
-        match action.dir {
-            'U' => head_y -= 1,
-            'D' => head_y += 1,
-            'L' => head_x -= 1,
-            'R' => head_x += 1,
-            _ => {}
+    fn advance(&self, action: Action) -> State {
+        let mut it = self.knots.iter();
+        let new_head = move_head(it.next().unwrap(), action);
+        let mut new_knots = vec![new_head];
+        for knot in it {
+            let new_knot = follow(new_knots.last().unwrap(), knot);
+            new_knots.push(new_knot);
         }
         State {
-            head_pos: (head_x, head_y),
-            ..*self
+            knots: new_knots
         }
     }
 
-    fn move_tail(&self) -> State {
-        let (head_x, head_y) = self.head_pos;
-        let (mut tail_x, mut tail_y) = self.tail_pos;
-        let delta_x = head_x - tail_x;
-        let delta_y = head_y - tail_y;
-
-        if delta_x != 0 && delta_y != 0 && (delta_x.abs() > 1 || delta_y.abs() > 1) {
-            tail_x += delta_x.signum();
-            tail_y += delta_y.signum();
-        } else if delta_y == 0 && delta_x.abs() > 1 {
-            tail_x += delta_x.signum();
-        } else if delta_x == 0 && delta_y.abs() > 1 {
-            tail_y += delta_y.signum();
-        }
-
-        State {
-            tail_pos: (tail_x, tail_y),
-            ..*self
-        }
+    fn tail_pos(&self) -> (i32, i32) {
+        self.knots.last().unwrap().clone()
     }
+}
+
+fn move_head(knot: &(i32, i32), action: Action) -> (i32, i32) {
+    let (mut head_x, mut head_y) = knot;
+    match action.dir {
+        'U' => head_y -= 1,
+        'D' => head_y += 1,
+        'L' => head_x -= 1,
+        'R' => head_x += 1,
+        _ => {}
+    }
+    (head_x, head_y)
+}
+
+fn follow(leader: &(i32, i32), follower: &(i32, i32)) -> (i32, i32) {
+    let (head_x, head_y) = leader;
+    let (mut follow_x, mut follow_y) = follower;
+    let delta_x = head_x - follow_x;
+    let delta_y = head_y - follow_y;
+
+    if delta_x != 0 && delta_y != 0 && (delta_x.abs() > 1 || delta_y.abs() > 1) {
+        follow_x += delta_x.signum();
+        follow_y += delta_y.signum();
+    } else if delta_y == 0 && delta_x.abs() > 1 {
+        follow_x += delta_x.signum();
+    } else if delta_x == 0 && delta_y.abs() > 1 {
+        follow_y += delta_y.signum();
+    }
+
+    (follow_x, follow_y)
 }
 
 #[derive(Clone)]

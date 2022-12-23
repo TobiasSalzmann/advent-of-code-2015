86709018665d7a96837f7b159ac4e0bc65fc5284
tsalzman <tsalzman@thoughtworks.com>
2022-12-23 02:28:41 +0100

    Day 22!!!!!!!!!!!!!!!!!!

--- src/day20.rs
+++ src/day20.rs
@@ -1,5 +1,4 @@
 use itertools::{Itertools};
-use rayon::prelude::*;
 use crate::util;
 
 pub fn main() {
--- src/day22.rs
+++ src/day22.rs
@@ -1,8 +1,5 @@
 use std::collections::{HashMap, HashSet};
-use std::io::empty;
-use std::str::FromStr;
 use itertools::{Itertools};
-use pathfinding::prelude::Edge;
 use crate::day22::Instruction::{Forward, Left, Right};
 use crate::day22::Tile::{Empty, Rock};
 use crate::util;
@@ -31,8 +28,8 @@ fn cube_password(instructions: Vec<Instruction>, map: &HashMap<(i32, i32), Tile>
                 let left = CubeEdge { start: top_left, end: bottom_left, side: 2 };
                 let top = CubeEdge { start: top_left, end: top_right, side: 3 };
                 for e in [right, bottom, left, top] {
-                    corners.entry(e.start.clone()).or_insert(HashSet::new()).insert(e.clone());
-                    corners.entry(e.end.clone()).or_insert(HashSet::new()).insert(e.clone());
+                    corners.entry(vec![e.start.clone()]).or_insert(HashSet::new()).insert(e.clone());
+                    corners.entry(vec![e.end.clone()]).or_insert(HashSet::new()).insert(e.clone());
                     edges.push(e)
                 }
             }
@@ -40,8 +37,12 @@ fn cube_password(instructions: Vec<Instruction>, map: &HashMap<(i32, i32), Tile>
     }
 
     while let Some((c1, c2)) = find_mergable_corners(&corners, &edges) {
-        let edges = corners.remove(&c2).unwrap();
-        corners.get_mut(&c1).unwrap().extend(edges)
+        let edges1 = corners.remove(&c1).unwrap();
+        let edges2 = corners.remove(&c2).unwrap();
+        let mut new_key = c1.clone();
+        new_key.extend(c2);
+        new_key.sort();
+        corners.insert(new_key, edges1.union(&edges2).cloned().collect());
     }
 
     assert!(corners.len() == 8);
@@ -56,16 +57,70 @@ fn cube_password(instructions: Vec<Instruction>, map: &HashMap<(i32, i32), Tile>
         .filter_map(|(_, v)| v.cloned().collect_tuple::<(CubeEdge, CubeEdge)>())
         .collect_vec();
 
-    for edge_pair in edge_pairs {
-        println!("{:?}\n{:?}\n", edge_pair.0, edge_pair.1)
+    // for edge_pair in edge_pairs {
+    //     println!("{:?}", get_corners(&corners, &edge_pair.0).iter().sorted().collect_vec());
+    //     println!("{:?}", get_corners(&corners, &edge_pair.1).iter().sorted().collect_vec());
+    //     println!("{:?}\n{:?}\n", edge_pair.0, edge_pair.1)
+    // }
+
+    let mut portals: HashMap<((i32, i32), i32), ((i32, i32), i32)> = HashMap::new();
+    for (e1, e2) in edge_pairs.clone() {
+        let corner = get_corners(&corners, &e1).iter().sorted().collect_vec()[0].clone();
+        let a = corner.contains(&e1.start);
+        let b = corner.contains(&e2.start);
+        let is_opposite = a & !b || !a && b;
+        if is_opposite {
+            for (k, v) in e1.to_points().iter().zip(e2.to_points_incoming().iter().rev()) {
+                portals.insert(k.clone(), v.clone());
+            }
+            for (k, v) in e2.to_points().iter().zip(e1.to_points_incoming().iter().rev()) {
+                portals.insert(k.clone(), v.clone());
+            }
+        } else {
+            for (k, v) in e1.to_points().iter().zip(e2.to_points_incoming().iter()) {
+                portals.insert(k.clone(), v.clone());
+            }
+            for (k, v) in e2.to_points().iter().zip(e1.to_points_incoming().iter()) {
+                portals.insert(k.clone(), v.clone());
+            }
+        }
     }
 
-
-
-    0
+    cube_password_with_portals(instructions, map, &portals)
 }
 
-fn find_mergable_corners(corners: &HashMap<(i32, i32), HashSet<CubeEdge>>, edges: &Vec<CubeEdge>) -> Option<((i32, i32), (i32, i32))> {
+fn cube_password_with_portals(instructions: Vec<Instruction>, map: &HashMap<(i32, i32), Tile>, portals: &HashMap<((i32, i32), i32), ((i32, i32), i32)>) -> i32 {
+    let (mut at_x, mut at_y) = initial_position(map);
+    let mut facing = 0;
+    for instruction in instructions {
+        match instruction {
+            Forward(n) => for _ in 0..n {
+                let try_next = match facing {
+                    0 => (at_x + 1, at_y),
+                    1 => (at_x, at_y + 1),
+                    2 => (at_x - 1, at_y),
+                    3 => (at_x, at_y - 1),
+                    _ => panic!()
+                };
+                let ((new_x, new_y), new_face) = match map.get(&try_next) {
+                    Some(_) => (try_next, facing),
+                    None => portals.get(&((at_x, at_y), facing)).unwrap().clone()
+                };
+                if let Some(Empty) = map.get(&(new_x, new_y)) {
+                    at_x = new_x;
+                    at_y = new_y;
+                    facing = new_face;
+                }
+            },
+            Left => { facing = (facing - 1 + 4) % 4; }
+            Right => { facing = (facing + 1) % 4; }
+        }
+    }
+
+    1000 * (at_y + 1) + 4 * (at_x + 1) + facing
+}
+
+fn find_mergable_corners(corners: &HashMap<Vec<(i32, i32)>, HashSet<CubeEdge>>, edges: &Vec<CubeEdge>) -> Option<(Vec<(i32, i32)>, Vec<(i32, i32)>)> {
     let outside_edges: Vec<CubeEdge> = edges
         .iter()
         .sorted_by_key(|e| get_corners(corners, &(**e).clone()).iter().cloned().sorted().collect_vec())
@@ -80,11 +135,11 @@ fn find_mergable_corners(corners: &HashMap<(i32, i32), HashSet<CubeEdge>>, edges
 
     for e1 in &outside_edges {
         for e2 in &outside_edges {
-            let e1_corners: HashSet<(i32, i32)> = get_corners(corners, e1);
-            let e2_corners: HashSet<(i32, i32)> = get_corners(corners, e2);
-            let intersection: Vec<&(i32, i32)> = e1_corners.intersection(&e2_corners)
+            let e1_corners: HashSet<Vec<(i32, i32)>> = get_corners(corners, e1);
+            let e2_corners: HashSet<Vec<(i32, i32)>> = get_corners(corners, e2);
+            let intersection: Vec<&Vec<(i32, i32)>> = e1_corners.intersection(&e2_corners)
                 .collect_vec();
-            if intersection.len() == 1 && corners.get(intersection[0]).unwrap().len()  == 6 {
+            if intersection.len() == 1 && corners.get(intersection[0]).unwrap().len() == 6 {
                 return e1_corners.symmetric_difference(&e2_corners)
                     .cloned()
                     .collect_tuple();
@@ -94,7 +149,7 @@ fn find_mergable_corners(corners: &HashMap<(i32, i32), HashSet<CubeEdge>>, edges
     None
 }
 
-fn get_corners(corners: &HashMap<(i32, i32), HashSet<CubeEdge>>, e1: &CubeEdge) -> HashSet<(i32, i32)> {
+fn get_corners(corners: &HashMap<Vec<(i32, i32)>, HashSet<CubeEdge>>, e1: &CubeEdge) -> HashSet<Vec<(i32, i32)>> {
     corners.iter()
         .filter(|(_, v)| v.contains(e1))
         .map(|(k, _)| k.clone())
@@ -106,6 +161,23 @@ struct CubeEdge {
     start: (i32, i32),
     end: (i32, i32),
     side: i32,
+}
+
+impl CubeEdge {
+    fn to_points(&self) -> Vec<((i32, i32), i32)> {
+        match self.side.clone() {
+            0 => (self.start.1..self.end.1).map(|y| ((self.start.0 - 1, y), 0)).collect_vec(),
+            1 => (self.start.0..self.end.0).map(|x| ((x, self.start.1 - 1), 1)).collect_vec(),
+            2 => (self.start.1..self.end.1).map(|y| ((self.start.0, y), 2)).collect_vec(),
+            3 => (self.start.0..self.end.0).map(|x| ((x, self.start.1), 3)).collect_vec(),
+            _ => panic!()
+        }
+    }
+
+    fn to_points_incoming(&self) -> Vec<((i32, i32), i32)> {
+        self.to_points().iter()
+            .map(|(p, d)| (p.clone(), (d.clone() + 2) % 4)).collect_vec()
+    }
 }
 
 
@@ -123,7 +195,6 @@ fn password(instructions: Vec<Instruction>, map: &HashMap<(i32, i32), Tile>) -> 
             Left => { facing = (facing - 1 + 4) % 4; }
             Right => { facing = (facing + 1) % 4; }
         }
-        println!("After {:?}: {:?}", instruction, (at_x, at_y, facing));
     }
 
     1000 * (at_y + 1) + 4 * (at_x + 1) + facing
@@ -194,7 +265,7 @@ fn up(map: &HashMap<(i32, i32), Tile>, at_x: i32, at_y: i32) -> ((i32, i32), Til
 
 fn initial_position(map: &HashMap<(i32, i32), Tile>) -> (i32, i32) {
     map.iter()
-        .filter(|((x, y), t)| *y == 0 && **t == Empty)
+        .filter(|((_, y), t)| *y == 0 && **t == Empty)
         .map(|(p, _)| p)
         .min()
         .unwrap()
@@ -325,6 +396,6 @@ mod tests {
         ];
         let (map, is) = parse(input);
 
-        assert_eq!(cube_password(is, &map), 6032)
+        assert_eq!(cube_password(is, &map), 5031)
     }
 }

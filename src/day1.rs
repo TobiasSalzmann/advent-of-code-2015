cb81cc62f81d19cb91dfb46ac18c1ce719b2e29e
tsalzman <tsalzman@thoughtworks.com>
2022-12-28 01:11:10 +0100

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,60 +1,34 @@
 use crate::util;
-use itertools::Itertools;
 
 pub fn main() {
-    let input = util::parse_int_lists("resources/day1.txt");
+    let input = util::parse_strings("resources/day1.txt").first().unwrap().clone();
 
-    println!("Day 1, Part 1: {:?} calories", max_calories(input.clone()));
-    println!("Day 1, Part 2: {:?} calories", max_3_calories(input));
+    println!("Day 1, Part 1: {:?}", final_floor(&input));
+    println!("Day 1, Part 2: {:?}", basement_index(&input));
+    // println!("Day 1, Part 2: {:?} calories", max_3_calories(input));
 }
 
-fn max_calories(calorie_lists: Vec<Vec<i32>>) -> i32 {
-    calorie_lists.iter()
-        .map(|calorie_list| calorie_list.iter().sum())
-        .max()
-        .unwrap()
+fn final_floor(instructions: &String) -> u32 {
+    let mut floor = 0;
+    for c in instructions.chars() {
+        match c {
+            '(' => floor += 1,
+            _ => floor -= 1,
+        }
+    }
+    floor
 }
 
-fn max_3_calories(calorie_lists: Vec<Vec<i32>>) -> i32 {
-    calorie_lists
-        .into_iter()
-        .map(|calorie_list| calorie_list.iter().sum())
-        .sorted_by_key(|x: &i32| -x)
-        .take(3)
-        .sum()
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::day1::{max_3_calories, max_calories};
-
-    #[test]
-    fn find_max_total_of_lists() {
-        let calorie_lists: Vec<Vec<i32>> = vec![
-            vec![43],
-            vec![2, 20, 22],
-            vec![1, 1, 1, 1, 1],
-        ];
-        assert_eq!(max_calories(calorie_lists), 44);
+fn basement_index(instructions: &String) -> usize {
+    let mut floor = 0;
+    for (i, c) in instructions.chars().enumerate() {
+        match c {
+            '(' => floor += 1,
+            _ => floor -= 1,
+        }
+        if floor == -1 {
+            return i + 1;
+        }
     }
-
-    #[test]
-    fn find_max_total_of_simple_lists() {
-        let calorie_lists: Vec<Vec<i32>> = vec![
-            vec![43],
-        ];
-        assert_eq!(max_calories(calorie_lists), 43);
-    }
-
-
-    #[test]
-    fn find_top_3_total_of_simple_lists() {
-        let calorie_lists: Vec<Vec<i32>> = vec![
-            vec![1],
-            vec![1, 1, 1, 1],
-            vec![1, 1],
-            vec![1, 1, 1],
-        ];
-        assert_eq!(max_3_calories(calorie_lists), 9);
-    }
+    panic!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
 mod util;
 mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
-mod day15;
-mod day17;
-mod day18;
-mod day16;
-mod day19;
-mod day20;
-mod day21;
-mod day22;
-mod day23;
-mod day24;
-mod day25;
 
 extern crate dotenv;
 extern crate core;
@@ -54,30 +30,6 @@ fn run(day: i32, time: bool) {
     let start = Instant::now();
     match day {
         1 => day1::main(),
-        2 => day2::main(),
-        3 => day3::main(),
-        4 => day4::main(),
-        5 => day5::main(),
-        6 => day6::main(),
-        7 => day7::main(),
-        8 => day8::main(),
-        9 => day9::main(),
-        10 => day10::main(),
-        11 => day11::main(),
-        12 => day12::main(),
-        13 => day13::main(),
-        14 => day14::main(),
-        15 => day15::main(),
-        16 => day16::main(),
-        17 => day17::main(),
-        18 => day18::main(),
-        19 => day19::main(),
-        20 => day20::main(),
-        21 => day21::main(),
-        22 => day22::main(),
-        23 => day23::main(),
-        24 => day24::main(),
-        25 => day25::main(),
         other => { println!("Day {} not yet implemented 😅", other) }
     }
     let duration = start.elapsed();

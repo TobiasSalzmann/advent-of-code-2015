9c38985ee933eb6eae6fa65234f3506c8187cf79
tsalzman <tsalzman@thoughtworks.com>
2023-01-03 12:55:58 +0100

    Day 12

---
+++ src/day12.rs
@@ -0,0 +1,26 @@
+use serde_json::{json, Result, Value};
+use itertools::{Itertools};
+use serde_json::Value::Number;
+
+use crate::util;
+
+pub fn main() {
+    let input = util::parse_string("resources/day12.txt");
+    let json: Value = serde_json::from_str(&input).unwrap();
+
+
+    println!("Day 11 Part 1: {}", add_numbers(&json, false));
+    println!("Day 11 Part 2: {}", add_numbers(&json, true));
+}
+
+fn add_numbers(json: &Value, ignore_red: bool) -> i64 {
+    match json {
+        Value::Number(n) => n.as_i64().unwrap() ,
+        Value::String(_) => 0,
+        Value::Array(values) => values.iter().map(|v| add_numbers(v, ignore_red)).sum(),
+        Value::Object(mappings) if ignore_red
+            && mappings.values().contains(&Value::String("red".to_string())) => 0,
+        Value::Object(mappings) => mappings.values().map(|v| add_numbers(v, ignore_red)).sum(),
+        _ => panic!(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod day8;
 mod day9;
 mod day10;
 mod day11;
+mod day12;
 
 extern crate dotenv;
 extern crate core;
@@ -50,6 +51,7 @@ fn run(day: i32, time: bool) {
         9 => day9::main(),
         10 => day10::main(),
         11 => day11::main(),
+        12 => day12::main(),
         other => { println!("Day {} not yet implemented 😅", other) }
     }
     let duration = start.elapsed();

1e2a54a6e20b63cde2cd9b11e8a388f3ca33a70d
tsalzman <tsalzman@thoughtworks.com>
2023-02-19 17:39:48 +0100

    Day19

--- src/day19.rs
+++ src/day19.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use itertools::Itertools;
 
 use crate::util;
@@ -8,6 +9,7 @@ pub fn main() {
 
 
     println!("Day 19 Part 1: {}", count_replacements(&transformations, &molecule));
+    println!("Day 19 Part 2: {}", cheapest_match(&transformations, &molecule))
 }
 
 fn count_replacements(transformations: &Vec<(String, Vec<String>)>, molecule: &Vec<String>) -> usize {
@@ -44,4 +46,82 @@ fn separate(molecule: String) -> Vec<String> {
         .split_whitespace()
         .map(|s| s.to_string())
         .collect_vec()
+}
+
+#[derive(Clone, Hash, Ord, PartialOrd, Eq, PartialEq, Debug)]
+struct Match {
+    start: usize,
+    end: usize,
+    atom: String,
+}
+
+type Atom = String;
+type Molecule = Vec<Atom>;
+
+fn cheapest_match(transformations: &Vec<(Atom, Molecule)>, molecule: &Molecule) -> usize {
+    let mut matches: HashMap<Match, usize> = molecule.iter()
+        .enumerate()
+        .map(|(i, s)| (Match {
+            start: i,
+            end: i+1,
+            atom: s.clone(),
+        }, 0)).collect();
+    for match_length in 2..=molecule.len() {
+        let mut number_of_matches = matches.len();
+        loop {
+            for start in 0..molecule.len()-match_length+1 {
+                let end = start + match_length;
+                for (atom, seq) in transformations {
+                    if let Some(cheapest_split) = get_cheapest_split(start, end,seq.as_slice(), &matches) {
+                        let m = Match {
+                            start,
+                            end,
+                            atom: atom.clone(),
+                        };
+                        matches.insert(m, cheapest_split + 1);
+                    }
+                }
+            }
+            if number_of_matches == matches.len() {
+                break
+            } else {
+                number_of_matches = matches.len()
+            }
+        }
+    }
+
+    matches.get(&Match{
+        start: 0,
+        end: molecule.len(),
+        atom: "e".to_string(),
+    }).unwrap().clone()
+}
+
+fn get_cheapest_split(start: usize, end: usize, seq: &[Atom], costs: &HashMap<Match, usize>) -> Option<usize> {
+    if seq.is_empty() && start == end {
+        return Some(0);
+    }
+    if seq.is_empty() {
+        return None;
+    }
+    if start >= end {
+        return None
+    }
+    let mut min: Option<usize> = None;
+    for first_end in start+1..=end {
+        let m = Match {
+            start,
+            end: first_end,
+            atom: seq[0].clone(),
+        };
+        if let Some(first_costs) = costs.get(&m) {
+            if let Some(c) =  get_cheapest_split(first_end, end, &seq[1..], costs) {
+                let total_costs = first_costs + c;
+                if min.is_none() || min.unwrap() > total_costs {
+                    min = Some(total_costs)
+                }
+            }
+        }
+    }
+    min
 }
